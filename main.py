fda2ceb4736363b39db2f30bfcb5a72930720e6b
asilva-juarez <asilva-juarez@myseneca.ca>
2025-08-28 23:21:28 -0400

    Testing detect.py and clustering.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 	--> SnapStock V 1.1 Update <--
 
-	NOTE: The project idea is generalize and will be hard to achieve it doing that
+	NOTE: The project idea is generalized and will be hard to achieve it doing that
 	for every sector, therefore, we are going to do it for a bakery store, in that way we
 	can follow the structure and adjust the dataset for any sector.
 
@@ -32,10 +32,55 @@
 		- /generate-db: This endpoint takes the user's labels for the clusters and calls
 		the LLM script to generate and return the final database script.
 """
+import os
 
+import cv2
 from detect import detect_and_filter_objects
+from clustering import coords_to_midpoints, visualize_clusters
+from sklearn.cluster import DBSCAN
 
-boxes = detect_and_filter_objects("./test_images_irl/tims-01.jpeg")
+"""
+	This file is for testing any new features added 
+	following the workflow of the project
+"""
 
-print(boxes[0])
+img_path = "./test_images_irl/tims-01.jpeg"
+original_image = cv2.imread(img_path)
+"""
+	Detecting objects in the image and getting their bounding boxes based
+	on confidence score (default 0.5)
+"""
+
+results = detect_and_filter_objects(img_path)
+
+"""
+	Calculate the center coordinates of the bounding boxes to apply
+	DBSCAN for clustering
+"""
+
+midpoints_results = []
+
+for boxes in results:
+	midpoints = coords_to_midpoints(boxes)
+	midpoints_results.append(midpoints)
+
+"""
+	Applying DBSCAN algorithm
+"""
+
+cluster = DBSCAN(eps=100, min_samples=1)
+
+for i, midpoints in enumerate(midpoints_results):
+	cluster_labels = cluster.fit_predict(midpoints)
+
+	print("Visualizing results...")
+	output_image = visualize_clusters(original_image.copy(), results[i], cluster_labels)
+
+	# Construct the output filename
+	base_name = os.path.splitext(os.path.basename('./test_images_irl/tims_DBSCAN.jpeg'))[0]
+	output_filename = f"{base_name}_clustered.jpg"
+
+	# Save the final image
+	cv2.imwrite(output_filename, output_image)
+	print(f"Successfully saved clustered image to: {output_filename}")
 
